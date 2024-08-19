1999bb63a9545e2ac85dacdc364f6fe97a1d08d5
Pomelo Orange <jingzonglei@163.com>
2024-08-19 23:56:48 +0800

    Create BorderTheme.ts

---
+++ src/themes/BorderTheme.ts
@@ -0,0 +1,33 @@
+import { Theme } from "./types";
+
+/**
+ * Statuses supported by the Border theme.
+ * {@link https://github.com/Akifyss/obsidian-border}
+ */
+export const ThingsTheme: Theme = {
+  name: 'Border Theme',
+  statuses: [
+    // Basic
+    { character: ' ', title: 'To Do' },
+    { character: '/', title: 'In Progress' },
+    { character: 'x', title: 'Done' },
+    { character: '-', title: 'Canceled' },
+    { character: '>', title: 'Rescheduled' },
+    { character: '<', title: 'Scheduled' },
+    // Extras
+    { character: '!', title: 'Important' },
+    { character: '?', title: 'Question' },
+    { character: 'i', title: 'Information' },
+    { character: 'S', title: 'Amount' },
+    { character: '*', title: 'Star' },
+    { character: 'b', title: 'Bookmark' },
+    { character: '"', title: 'Quote' },
+    { character: 'n', title: 'Note' },
+    { character: 'l', title: 'Location' },
+    { character: 'I', title: 'Idea' },
+    { character: 'p', title: 'Pro' },
+    { character: 'c', title: 'Con' },
+    { character: 'u', title: 'up' },
+    { character: 'd', title: 'down' },
+  ]
+};
