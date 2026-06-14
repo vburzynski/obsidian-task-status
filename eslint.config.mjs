24312ebb413324227e0cf8a92348198e7af80cef
Valerie Burzynski <valerieburzynski@gmail.com>
2026-06-14 14:00:50 -0500

    build: update npm package dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,9 +18,6 @@ export default tseslint.config(
     rules: {
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": ["error", { "args": "none" }],
-      "@typescript-eslint/ban-ts-comment": "off",
-      "no-prototype-builtins": "off",
-      "@typescript-eslint/no-empty-function": "off"
     }
   },
   {
